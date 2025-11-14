ab3453db28c3c6eb4dc252671acb7c4b6118a4ce
MaëlieA <maelie@addario.fr>
2025-11-14 08:49:21 +0100

    maj node et fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 import node from "@astrojs/node";
 
 // Do not set `site` to an empty string — leave unset in dev.
 // Set to your production URL when deploying, e.g. site: 'https://monportfolio.fr'
 export default defineConfig({
   output: "server",
+  integrations: [tailwind()],
+
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [tailwind()],
 });
