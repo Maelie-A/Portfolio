d3005d01989ec2759821b9758cbb06cd484f4f34
MaëlieA <maelie@addario.fr>
2025-11-14 08:40:25 +0100

    ajout node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // Do not set `site` to an empty string — leave unset in dev.
 // Set to your production URL when deploying, e.g. site: 'https://monportfolio.fr'
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [tailwind()],
 });
